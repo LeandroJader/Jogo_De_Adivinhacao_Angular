7c34aa83b1cc0eb51b345388cc906315b669e891
LeziinFx <leandronunesramosln@gmail.com>
2025-11-06 19:39:04 -0300

    Implementada versão inicial do jogo de adivinhação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,54 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit,} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('jogo-Da-Adivinhacao');
+export class App implements OnInit {
+
+ public numeroSecreto : number = 0;
+ public numeroDigitado : number = 1;
+
+public jogoEstaFinalizado :boolean = false;
+
+ public DicaNumeroMaiorQue:number = 1;
+ public DicaNumeromenorQue:number = 100;
+
+  ngOnInit(): void {
+     this.numeroSecreto = this.obterNumeroSecreto()
+  }
+
+private obterNumeroSecreto() {
+
+  const numeroAleatorio : number = Math.random() * 100;
+
+   const numeroSecreto = Math.floor(numeroAleatorio)
+
+   console.log(numeroSecreto)
+    
+  return Math.floor(numeroAleatorio);
+}
+
+ public adivinhar (){
+if(this.numeroDigitado < this.numeroSecreto)
+this.DicaNumeroMaiorQue = this.numeroDigitado
+
+else if (this.numeroDigitado > this.numeroSecreto)
+  this.DicaNumeromenorQue = this.numeroDigitado
+
+else{
+  this.jogoEstaFinalizado = true;
+}
+ }
+ public reiniciar(){
+  this.numeroDigitado = 1;
+  this.DicaNumeroMaiorQue =1;
+  this.DicaNumeromenorQue = 100;
+  this.jogoEstaFinalizado = false;
+  
+  this.numeroSecreto = this.obterNumeroSecreto()
+ }
 }
