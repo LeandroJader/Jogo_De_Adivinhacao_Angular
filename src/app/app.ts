44393b747049a04015c4f1e63f829a1ca2a34d91
LeziinFx <leandronunesramosln@gmail.com>
2025-11-07 14:49:57 -0300

    Implementada nova versão do jogo de adivinhação com sistema de ranking das últimas partidas jogadas e melhorias na estilização para compatibilidade com dispositivos móveis.

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,12 +15,44 @@ export class App implements OnInit {
   public DicaNumeroMaiorQue: number = 1;
   public DicaNumeromenorQue: number = 100;
 
-  ngOnInit(): void {
-    this.numeroSecreto = this.obterNumeroSecreto();
+  public DificuldadeSelecionada?: string;
+
+  public tentativasRestantes: number = 0;
+
+  public pontuacao: number = 100;
+
+  public rodadasAnteriores: number[] = [];
+
+  ngOnInit(): void {}
+
+  public selecionarDificuldade(dificuldade: string): void {
+    switch (dificuldade) {
+      case 'facil':
+        this.numeroSecreto = this.obterNumeroSecreto(10);
+        this.DicaNumeromenorQue = 10;
+        this.tentativasRestantes = 3;
+        break;
+
+      case 'medio':
+        this.numeroSecreto = this.obterNumeroSecreto(50);
+        this.DicaNumeromenorQue = 50;
+        this.tentativasRestantes = 6;
+        break;
+
+      case 'dificil':
+        this.numeroSecreto = this.obterNumeroSecreto(100);
+        this.DicaNumeromenorQue = 100;
+        this.tentativasRestantes = 7;
+        break;
+    }
+
+    this.DificuldadeSelecionada = dificuldade;
+
+    console.log(this.DificuldadeSelecionada);
   }
 
-  private obterNumeroSecreto() {
-    const numeroAleatorio: number = Math.random() * 100;
+  private obterNumeroSecreto(max: number) {
+    const numeroAleatorio: number = Math.random() * (max - 1) + 1;
 
     const numeroSecreto = Math.floor(numeroAleatorio);
 
@@ -30,19 +62,41 @@ export class App implements OnInit {
   }
 
   public adivinhar() {
+    this.tentativasRestantes--;
+
+    if (this.tentativasRestantes <= 0) {
+      this.jogoEstaFinalizado = true;
+      return;
+    }
+
     if (this.numeroDigitado < this.numeroSecreto) this.DicaNumeroMaiorQue = this.numeroDigitado;
     else if (this.numeroDigitado > this.numeroSecreto)
       this.DicaNumeromenorQue = this.numeroDigitado;
     else {
       this.jogoEstaFinalizado = true;
+      this.RegistrarPontuacao();
     }
+
+    const diferencaNumerica: number = Math.abs(this.numeroSecreto - this.numeroDigitado);
+
+    if (diferencaNumerica >= 10) this.pontuacao -= 10;
+    else if (diferencaNumerica >= 5) this.pontuacao -= 5;
+    else this.pontuacao -= 2;
   }
+
   public reiniciar() {
     this.numeroDigitado = 1;
     this.DicaNumeroMaiorQue = 1;
     this.DicaNumeromenorQue = 100;
     this.jogoEstaFinalizado = false;
 
-    this.numeroSecreto = this.obterNumeroSecreto();
+    this.DificuldadeSelecionada = undefined;
+    this.pontuacao = 100;
+  }
+  public RegistrarPontuacao() {
+    this.rodadasAnteriores.push(this.pontuacao);
+
+    if (this.rodadasAnteriores.length > 3) this.rodadasAnteriores.shift();
+    console.log(this.pontuacao);
   }
 }
